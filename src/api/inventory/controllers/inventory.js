83329afe76e1b150512e74aa91447a4240bd3209
Minh Dao <daonhatminh@gmail.com>
2023-08-11 17:43:19 +1000

    Extend inventory controller to customise find API

--- src/api/inventory/controllers/inventory.js
+++ src/api/inventory/controllers/inventory.js
@@ -6,4 +6,46 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::inventory.inventory');
+module.exports = createCoreController('api::inventory.inventory', ({strapi}) => ({
+  
+  // Extend controlled to retrieve only desired fields
+  async find(ctx) {
+    // @ts-ignore
+    const { data, meta } = await super.find(ctx);
+
+    const transformedData = await Promise.all(
+      data.map(async (inventoryEntry) => {
+        const inventoryEntryId = inventoryEntry.id;
+        const quantity = inventoryEntry.attributes.quantity;
+
+        // Get related item
+        const itemDetails = await strapi.db.query('api::item.item').findOne({
+          where: {
+            inventory: inventoryEntryId
+          },
+          populate: {
+            // @ts-ignore
+            image: true,
+          },
+        })
+        
+        // Filter for only desired attributes
+        const { id, price, name, image } = itemDetails;
+        const transformedItemDetails = {
+          itemId: id,
+          price: price,
+          name: name,
+          imageUrl: image[0].url
+        }
+
+        return {
+          inventoryEntryId,
+          quantity,
+          ...transformedItemDetails,
+        };
+      })
+    );
+
+    return { data: transformedData, meta };
+  }
+}));
